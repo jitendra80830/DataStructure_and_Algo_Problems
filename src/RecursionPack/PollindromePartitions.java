ca8f5eec7c749f6b510b3a6011cb91de625c948a
jitendra <jitendrakumar89973@gmail.com>
2021-07-17 16:25:21 +0530

    Pollindrome 2D arraylist

--- src/RecursionPack/PollindromePartitions.java
+++ src/RecursionPack/PollindromePartitions.java
@@ -1,5 +1,7 @@
 package RecursionPack;
 
+import java.util.ArrayList;
+
 public class PollindromePartitions {
     public static void main(String[] args){
         partition("nitin","");
@@ -29,5 +31,21 @@ public class PollindromePartitions {
             j--;
         }
         return true;
+    }
+    public static void partitionLeetCode(String ques , ArrayList<String> list, ArrayList<ArrayList<String>> ans){
+        if(ques.length() == 0){
+            ans.add(list);
+            return;
+        }
+
+        for (int i =1;i<=ques.length();i++){
+            String roq = ques.substring(i); //rest of ques
+            String roa = ques.substring(0,i); //rest of ans
+
+            if(isPollindrome(roa)){
+                list.add(roa);
+                partitionLeetCode(roq,list,ans);
+            }
+        }
     }
 }
