3f52d1f7bbe4df7b7b234927bb51eafa40c05b42
jitendra <jitendrakumar89973@gmail.com>
2021-11-08 11:48:59 +0530

    Tree

---
+++ src/BinaryTreePack/Mirror.java
@@ -0,0 +1,38 @@
+package BinaryTreePack;
+
+public class Mirror {
+    public class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null){
+                return null;
+            }
+
+            //for left call
+            TreeNode left = invertTree(root.left);
+
+            //for right call
+            TreeNode right = invertTree(root.right);
+
+            //asigning right child's adress in left
+            root.left = right;
+
+            //asigning left  child's adress in righ t
+            root.right = left;
+            return root;
+
+
+        }
+    }
+}
--- src/CodeForces/A.java
+++ src/CodeForces/A.java
@@ -1,16 +1,57 @@
 package CodeForces;
 
-public class A {
-    public static void main(String[] args) {
-        for (int i = 7; i >= 1; i--) {
-            for (int k = 7; k >= i; k--) {
-                System.out.print(" ");  // Print 7-i number of spaces before start of each line
+import java.io.*;
+import java.util.*;
+
+//public class A{
+//
+//    public static void main(String args[])throws Exception
+//    {
+//        BufferedReader bu=new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb=new StringBuilder();
+//        int t=Integer.parseInt(bu.readLine());
+//        while(t-->0)
+//        {
+//            char p[]=bu.readLine().toCharArray();
+//            int i,c[]=new int[26];
+//            for(i=0;i<26;i++) c[p[i]-'a']=i;
+//
+//            char s[]=bu.readLine().toCharArray();
+//            int ans=0,n=s.length;
+//            for(i=1;i<n;i++) ans+=Math.abs(c[s[i]-'a']-c[s[i-1]-'a']);
+//            sb.append(ans+"\n");
+//        }
+//        System.out.print(sb);
+//    }
+//}
+ public class A{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s  = new String();
+        int t = Integer.parseInt(bufferedReader.readLine());
+        while(t > 0){
+            char p1[] = bufferedReader.readLine().toCharArray();
+            int c1[] = new int[26];
+
+            for(int i =0;i<26;i++){
+                c1[p1[i] - 'a'] = i;
+                char s1[] = bufferedReader.readLine().toCharArray();
+                int answer = 0;
+                int n = s1.length;
+
+                for(int j =1;j<n;j++){
+                    answer+=Math.abs(c1[s1[i] - 'a'] - c1[s1[j-1] - 'a']);
+                    s.concat(answer+"\n");
+                }
             }
-            System.out.print(i); // Print i
-            for (int j = 1; j <= i*2; j++) {
-                System.out.print(" "); // Print i*2 number of spaces after printing i
-            }
-            System.out.println(i); // Print i
+
+            t--;
         }
+        System.out.print(s);
     }
+
 }
+//abcdefghijklmnopqrstuvwxyz
+//hello   h =8,e=5 , l = 12 , o = 15
+//5-8,
+//|5−8|+|12−5|+|12−12|+|15−12|=13
